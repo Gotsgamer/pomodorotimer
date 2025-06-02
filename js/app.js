88a9e4dd826261cd411e7cdd4b483e733f3a701c
Gotsgamer <94848890+Gotsgamer@users.noreply.github.com>
2025-06-01 17:07:18 -0700

    Add files via upload
    this is the final change the timer should be done with this

--- js/app.js
+++ js/app.js
@@ -3,10 +3,15 @@ const start = document.getElementById("start");
 const stop = document.getElementById("stop");
 const reset = document.getElementById("reset");
 
-let timeLeft = 1500
+let timeLeft = 5
 let interval;
 let breaktime = 0; //the intention of this is to start a 5min timer AFTER the 25 min timer
 let barrier = 0; //this literally only exits bsc if you spam the start button it increases the amount the time left is decreased per click and idk how to fix it
+
+// i set these up bsc I decided that inverted the colors on break time is just a cool idea :)
+const body = document.body;
+const container = document.querySelector(".container");
+
 
 const shorttimer = () => {
   clearInterval(interval);
@@ -15,6 +20,9 @@ const shorttimer = () => {
   barrier = 0
   alert("breaktime!");
   breaktime = 0
+  body.style.background = "linear-gradient(-135deg, #E58EE6CC, #E58EE6CC)";
+  container.style.backgroundColor = "#232425FF";
+
 }
 const longtimer = () => {
   clearInterval(interval);
@@ -23,6 +31,8 @@ const longtimer = () => {
   barrier = 0
   alert("GRIND TIME BABYYYY!");
   breaktime = 1
+  body.style.background = "linear-gradient(-135deg, #3A3C41FF, #232425FF)";
+  container.style.backgroundColor = "#E58EE6CC";
 }
 
 const blocker = () => {
@@ -91,3 +101,11 @@ const resetTimer = () => {
 start.addEventListener("click", startTimer);
 stop.addEventListener("click", stopTimer);
 reset.addEventListener("click",resetTimer);
+
+// ngl, making that barrier was difficult, in application its simple but
+//what I realized with variables like this the more you rely on them
+//the more debugging you have to do
+//like whne I added the time switching I had to change how my barrier
+//varible acted in response.
+//while my variable usage did work in this attempt its not scalealbe
+//I need to find out how I can find inbuilt elements or keywords that do what I want that way I don't need vairables. 
